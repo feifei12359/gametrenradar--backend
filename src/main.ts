e9b4df97888cedb5c5c6d9318cd22c446476dccb
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-04 20:42:12 +0800

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  // 开启 CORS，允许前端跨域访问
+  app.enableCors({
+    origin: "*",
+  })
+
   const port = process.env.PORT || 3000
 
   await app.listen(port)
