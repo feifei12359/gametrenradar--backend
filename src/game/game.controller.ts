aeff16fbb21911d476e91157a16a506c55d5c07c
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-04 20:20:18 +0800

    Add files via upload

---
+++ src/game/game.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('games')
+export class GameController {
+
+  @Get()
+  getGames() {
+    return [
+      { id: 1, name: "Test Game 1" },
+      { id: 2, name: "Test Game 2" }
+    ];
+  }
+
+}
