19e7b49ba6dfc864c047ee9ecd947084015e4a2f
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-06 22:25:40 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const appModule = require('./app.module');
 async function bootstrap() {
   const app = await core.NestFactory.create(appModule.AppModule);
 
+  // ✅ 开启跨域访问
   app.enableCors({
     origin: [
-      'https://game-trend-radar-qianduan.vercel.app', // 允许前端
-      'http://localhost:3000', // 本地开发允许
+      'https://game-trend-radar-qianduan.vercel.app', // 允许你的前端域名
+      'http://localhost:3000', // 本地开发也允许
     ],
     methods: ['GET', 'POST'],
     credentials: true,
