6915d6698b95a961e1c67baa6956aeb2df02cb19
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-08 19:47:17 +0800

    Add files via upload

--- health/health.controller.ts
+++ health/health.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('health') // 配合全局前缀 api，最终路由是 /api/health
+@Controller('health') // 注意不要写 /api/health
 export class HealthController {
   @Get()
   healthCheck() {
