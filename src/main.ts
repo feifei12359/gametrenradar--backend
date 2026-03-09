4e72c98197f2df84a6e54aab5d41bd0e5d953d01
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-09 21:13:47 +0800

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 开放跨域，前端 Vercel + 本地都可访问
+  // 允许跨域访问
   app.enableCors({
-    origin: [' `https://game-trend-radar-qianduan.vercel.app` ', 'http://localhost:3000'],
+    origin: [
+      ' `https://game-trend-radar-qianduan.vercel.app` ', // 生产前端
+      'http://localhost:3000',                        // 本地调试
+    ],
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     credentials: true,
   });
