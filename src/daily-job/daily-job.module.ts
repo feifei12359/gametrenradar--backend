1f0688b35335948d0aaf93ca17ab36aa1563198c
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-12 19:34:07 +0800

    Delete src/daily-job directory

--- src/daily-job/daily-job.module.ts
+++
@@ -1,8 +0,0 @@
-// backend/src/daily-job/daily-job.module.ts
-import { Module } from '@nestjs/common';
-import { DailyJobService } from './daily-job.service';
-
-@Module({
-  providers: [DailyJobService],
-})
-export class DailyJobModule { }
