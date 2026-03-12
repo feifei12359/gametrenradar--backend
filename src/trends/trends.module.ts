2a155eee8f25d867e92913d8cbcc0c12f5406308
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-12 19:39:11 +0800

    Delete src/trends directory

--- src/trends/trends.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { TrendsService } from './trends.service'
-
-@Module({
-  providers: [TrendsService],
-  exports: [TrendsService],
-})
-export class TrendsModule {}
