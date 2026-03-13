ab908c49fd9720885c2ff58e17c4a6173751c4a6
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-13 18:35:05 +0800

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/tests/**/*.spec.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  clearMocks: true,
-};
